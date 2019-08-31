3b9f550839e7c5fce2347d8a17af6a36acf1b3c3
Kartik Patkar <45987061+Kartikpatkar@users.noreply.github.com>
2019-08-31 15:36:51 +0530

    Create Alfalfa01Activity.java

---
+++ app/src/main/java/com/example/vitamindtrackerlite/Alfalfa01Activity.java
@@ -0,0 +1,14 @@
+package com.example.kartikpatkar.vitamindtrackerlite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Alfalfa01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alfalfa01);
+        setTitle("Alfalfa seeds, sprouted");
+    }
+}
